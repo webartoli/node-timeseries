ccb5de927f99a59700c3eaaf897566cf8a132ba9
Francesco Strazzullo <me@francescostrazzullo.info>
2015-07-01 13:15:18 +0200

    added gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var watch = require('gulp-watch');
+
+gulp.task('test', function () {
+    return gulp.src('tests/**/*.js').pipe(mocha());
+});
+
+gulp.task('default', function () {
+	return gulp.watch(['*.js','tests/*.js'], ['test']);
+});
