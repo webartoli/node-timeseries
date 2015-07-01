5369c662d07f7a9204f0b8e7035d7c8941e44b17
cbartoli <c.bartoli@iconsulting.biz>
2015-07-01 15:58:47 +0200

    Commit pre slides

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
+var moment = require('moment');
+var levelup = require('level');
 
-module.exports = {
-	
-	add : function(asset, value){
+module.exports = function build(dbPath) {
 		
-	},
+	var now;
+	var count;
 	
-	fetch : function(asset, start, end){
+	return {
+	
+		add : function(assetId, value, date) {
+			var current = Date.now();
+			
+			if (current === now) {
+				count++;
+			} else {
+				count = 0;
+			}
+			now = current;
+			date = date || moment();
+			
+			var keyTs = "ts::" + now + ":" + count;
+			var keyAs = "as::" + assetId + "--"+ now + ":" + count;
+			
+			db.put(keyTs, value);
+			db.put(keyAs + ":" + count,value);
+			
+		},
 		
-	}
-	
+		fetch : function( from, to, assetId) {
+			
+			var prefix;
+			
+			if (!assetId) {
+				assetId = assetId || "";
+				prefix = "ts::";
+			}
+			else {
+				prefix = "as::";
+			}	
+		}
+	};
 };
