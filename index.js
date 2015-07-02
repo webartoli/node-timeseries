ab218f8872cb575fe2bec1d960ccdf3033a8a2ea
cbartoli <c.bartoli@iconsulting.biz>
2015-07-02 14:32:22 +0200

    some test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var moment = require('moment');
+var through2 = require('through2');
 var Joi = require('joi');
 
 module.exports = function build(db) {
@@ -16,73 +17,100 @@ module.exports = function build(db) {
     now = current;
 
     return now;
-
   };
 
-  var valueSchema = Joi.object().keys({
+  var addValueSchema = Joi.object().keys({
     assetId: Joi.string().required(),
     date: Joi.date(),
     value: Joi.any().required()
   });
 
-  return {
+  function add(value, cb) {
 
-    add: function (value, cb) {
+    var validationResult = Joi.validate(value, addValueSchema);
 
-      Joi.validate(value, valueSchema, function (err, value) {
-        if (err === null) return;
-        console.error(err);
-      });
+    if (validationResult.error !== null) {
 
-      value.date = value.date || moment().toDate();
-
-      incCounter();
-
-      var formattedDate = Math.floor(value.date / 1000)
-
-      var keyTs = "ts::" + formattedDate + ":" + count;
-      var keyAs = "as::" + value.assetId + "--" + formattedDate + ":" + count;
-
-      db.batch()
-        .put(keyTs, value, { valueEncoding: 'json' })
-        .put(keyAs, value, { valueEncoding: 'json' })
-        .write(function () {
-        cb(null, value);
-      });
-    },
-
-    getDataStream: function (queryValues, cb) {
-
-      // validation
-
-      var gt;
-      var lt;
-      var prefix;
-      var options;
-      var assetId;
-
-      queryValues.from = queryValues.from || "";
-      queryValues.to = queryValues.to || '\xff';
-
-      if (!queryValues.assetId) {
-        prefix = "ts::";
-        assetId = "";
-      }
-      else {
-        prefix = "as::";
-        assetId = queryValues.assetId + "--";
-      }
-
-      gt = prefix + assetId + queryValues.from;
-      lt = prefix + assetId + queryValues.to;
-
-      options = {
-        valueEncoding: 'json',
-        lte: lt,
-        gte: gt
-      };
-
-      return db.createReadStream(options);
+      console.error("add validation error is", validationResult.error);
+      cb(validationResult.error);
+      return;
     }
+
+    value.date = value.date || moment().toDate();
+
+    incCounter();
+
+    var formattedDate = Math.floor(value.date / 1000)
+
+    var keyTs = "ts::" + formattedDate + ":" + count;
+    var keyAs = "as::" + value.assetId + "--" + formattedDate + ":" + count;
+
+    db.batch()
+      .put(keyTs, value, { valueEncoding: 'json' })
+      .put(keyAs, value, { valueEncoding: 'json' })
+      .write(function () {
+      cb(null, value);
+    });
+  }
+
+  var getQueryValuesSchema = Joi.object().keys({
+    assetId: Joi.string().required(),
+    from: Joi.date(),
+    to: Joi.date()
+  });
+
+  function getDataStream(queryValues) {
+    
+    var validationResult = Joi.validate(queryValues, getQueryValuesSchema);
+
+    if (validationResult.error !== null) {
+
+      console.error("getDataStream error is", validationResult.error);
+      
+      var stream = through2.obj()
+      
+      process.nextTick(function () {
+        stream.emit('error', validationResult.error)
+      })
+      
+      return stream;
+    }
+
+    var gt;
+    var lt;
+    var prefix;
+    var options;
+    var assetId;
+
+    queryValues.from = queryValues.from || "";
+    queryValues.to = queryValues.to || '\xff';
+
+    if (!queryValues.assetId) {
+      prefix = "ts::";
+      assetId = "";
+    }
+    else {
+      prefix = "as::";
+      assetId = queryValues.assetId + "--";
+    }
+
+    gt = prefix + assetId + queryValues.from;
+    lt = prefix + assetId + queryValues.to;
+
+    options = {
+      valueEncoding: 'json',
+      lte: lt,
+      gte: gt
+    };
+
+    return db.createReadStream(options);
+
+  }
+
+  return {
+    addSchema: function () { return addValueSchema; },
+    add: add,
+    getQuerySchema: function () { return getQueryValuesSchema; },
+    getDataStream: getDataStream
   };
 };
--- tests/test.js
+++ tests/test.js
@@ -3,51 +3,55 @@ var level = require('level-test')();
 var timeSeriesFactory = require("../index");
 var callbackStream = require("callback-stream");
 
-describe("timeSeries Module",function(){
-	
-	var db;
-	var count = 0;
-	
-	beforeEach(function (done) {
-		db = level('foo', {clean:true}, done);
-	});
-	
-	it("should work",function(done){
-		
-		var timeSeries = timeSeriesFactory(db);
+describe("timeSeries Module", function () {
 
-		var value = {
-			assetId:'1',
-			value:'value'
-		};
+  var db;
+  var count = 0;
 
-		timeSeries.add(value,function(err,returnValue){
-			assert.equal(returnValue.value,value.value);
-			done();
-		});
-	}); 
+  beforeEach(function (done) {
+    db = level('foo', { clean: true }, done);
+  });
 
-	it("should store",function(done){
-		var timeSeries = timeSeriesFactory(db);
+  it("should work", function (done) {
 
-		var value = {
-			assetId:'1',
-			value:'value'
-		};
+    var timeSeries = timeSeriesFactory(db);
 
-		timeSeries.add(value,function(err,returnValue){
-			var queryData = {
-				assetId:'1'
-			};
-			timeSeries.getDataStream({})
-			.pipe(callbackStream({ objectMode: true }, function(err,data){
-				assert.equal(data.length,1);	
-				done();
-			}));
-		});
-	});
-	
-	afterEach(function(done) {
-		db.close(done);
-	});
+    var value = {
+      assetId: '1',
+      value: 'value'
+    };
+
+    timeSeries.add(value, function (err, returnValue) {
+
+      assert.equal(returnValue.value, value.value);
+      done();
+    });
+  });
+
+  it("should store", function (done) {
+    var timeSeries = timeSeriesFactory(db);
+
+    var value = {
+      assetId: '1',
+      value: 'value'
+    };
+
+    timeSeries.add(value, function (err, returnValue) {
+      console.log('error is', err)
+      var queryData = {
+        assetId: '1'
+      };
+      timeSeries.getDataStream({})
+        .pipe(callbackStream({ objectMode: true }, function (err, data) {
+          // non passo da qui
+        console.log(">>>", err);
+        assert.equal(data.length, 1);
+        done();
+      }));
+    });
+  });
+
+  afterEach(function (done) {
+    db.close(done);
+  });
 });
